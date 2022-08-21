1cc0121fdb034e8a32cce63b9c593e6f4ad52b03
theRobMay <robbie.maysent@gmail.com>
2022-08-21 14:52:13 -0500

    changing objectives

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 require(`dotenv`).config();
 const md5 = require(`md5`);
-const private_key = '547f1b41988e4ab890e2ea3f69b9d90e7496df36';
+const private_key = '';
 const ts = new Date().getTime();
-const public_key = `7a40b956450425ab84960bc38b8bb0b8`;
+const public_key = ``;
 const hash = md5(ts+private_key+public_key);
-const apikey = public_key
 console.log(hash)
 
 // const fetchAPI{
 // //    fetch from API
 // }
+
+// calls function on page load
+window.onload = function() {
+    getBreweries();
+}
+
+// this function fetches the breweries
+const getBreweries = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cb85153293msh4e4af2f7a4897f2p146609jsn3b83daa74b08',
+            'X-RapidAPI-Host': 'brianiswu-open-brewery-db-v1.p.rapidapi.com'
+        }
+    }
+
+    fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    console.log(getBreweries)
+    // console.log(options)
+
+    fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries?by_state=NY&by_name=cooper&by_type=micro&by_tag=patio', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/autocomplete?query=dog', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+    fetch('https://brianiswu-open-brewery-db-v1.p.rapidapi.com/breweries/search?query=dog', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+
+};
